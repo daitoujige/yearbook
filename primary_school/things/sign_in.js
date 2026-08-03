1fdf5958c3099c76fbcd22e268294c314a499659
daitoujige <3665610192@qq.com>
2026-08-03 14:53:29 +0800

    Update sign_in.js

--- primary_school/things/sign_in.js
+++ primary_school/things/sign_in.js
@@ -3,7 +3,7 @@
         // 检查登录状态
         if (sessionStorage.getItem('is_logged_in') !== 'true') {
             // 使用绝对路径跳转（确保无论在哪一层都能找到 login.html）
-            window.location.replace('/login.html');
+            window.location.replace('https://daitoujige.github.io/yearbook/primary_school/login.html');
             // 阻止后续脚本继续执行
             throw new Error('未登录，已跳转');
         }
